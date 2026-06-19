cfd498860013d7c2c9536b6cd652afc5fafe8ba5
Enrico Weigelt, metux IT consult <info@metux.net>
2026-06-19 15:08:00 +0200

    Add xserver-25.2 MPBT config (config, devuan.yaml, packages symlinks, run scripts)
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

---
+++ cf/xserver-25.2/config.sh
@@ -0,0 +1,7 @@
+# Config sourced by run-* scripts for the xserver-25.2 release line.
+export XLIBRE_RELEASE="xserver-25.2"
+export PATH="$PATH:$HOME/go/bin"
+export MPBT="mpbt-builder"
+
+SOLUTION="cf/$XLIBRE_RELEASE/solutions/devuan.yaml"
+WORKDIR="_WORK_/$XLIBRE_RELEASE"
